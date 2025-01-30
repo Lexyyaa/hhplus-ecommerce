97af783ab8017d1f32ed369628ed8845a2864a4c
Lexyyaa <shw9910274@gmail.com>
2025-01-31 00:26:19 +0900

    fix : dto class type 변경(class->record)

--- src/main/java/com/hhplus/ecommerce/domain/point/UserPointCommand.java
+++ src/main/java/com/hhplus/ecommerce/domain/point/UserPointCommand.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserPointCommand {
 
     @Getter
--- src/main/java/com/hhplus/ecommerce/interfaces/dto/point/UserPointResponse.java
+++ src/main/java/com/hhplus/ecommerce/interfaces/dto/point/UserPointResponse.java
@@ -1,18 +1,13 @@
 package com.hhplus.ecommerce.interfaces.dto.point;
 
-import com.hhplus.ecommerce.domain.point.UserPoint;
 import com.hhplus.ecommerce.domain.point.UserPointInfo;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserPointResponse {
-    private Long userId;
-    private Long point;
-
+public record UserPointResponse (
+        Long userId,
+        Long point
+){
     public static UserPointResponse from(UserPointInfo point){
         return UserPointResponse.builder()
                 .userId(point.getUserId())
@@ -20,4 +15,3 @@ public class UserPointResponse {
                 .build();
     }
 }
-
